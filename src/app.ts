3e82a9f4a0bd90bd5b33ac59344a17fe751ecde9
sahariar8 <sahariaralam8@gmail.com>
2025-12-23 11:25:37 +0600

    create app.ts for clear view

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import initDB from "./config/db";
+import { vehicleRoutes } from "./modules/vehicle/vehicle.routes";
+import { userRoutes } from "./modules/user/user.routes";
+import { authRoutes } from "./modules/auth/auth.routes";
+import { bookingRoutes } from "./modules/booking/booking.routes";
+
+
+
+dotenv.config();
+const app = express();
+
+
+//parser
+app.use(express.json());
+
+//call DB initialization
+initDB();
+
+app.use("/api/v1/vehicles",vehicleRoutes);
+app.use("/api/v1/users",userRoutes);
+app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/bookings",bookingRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "Route Not Found",
+  });
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
+import app from "./app";
 import config from "./config";
-import initDB from "./config/db";
-import { vehicleRoutes } from "./modules/vehicle/vehicle.routes";
-import { userRoutes } from "./modules/user/user.routes";
-import { authRoutes } from "./modules/auth/auth.routes";
-import { bookingRoutes } from "./modules/booking/booking.routes";
 
-dotenv.config();
 
-const app = express();
 const port = config.port;
-
-
-//parser
-app.use(express.json());
-
-//call DB initialization
-initDB();
-
-app.use("/api/v1/vehicles",vehicleRoutes);
-app.use("/api/v1/users",userRoutes);
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/bookings",bookingRoutes);
-
-app.use((req, res) => {
-  res.status(404).json({
-    status: "error",
-    message: "Route Not Found",
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
